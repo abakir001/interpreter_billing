bb89bfe2171835cb4cd2ed7bc3814bb1b985f009
muratjumashev <jumasheff@gmail.com>
2019-01-10 12:39:55 +0600

    Customize admin panel

--- billing/admin.py
+++ billing/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from billing.forms import DepositForm
 from billing.models import Deposit
 from billing.models import Call
 
-admin.site.register(Deposit)
+
+class DepositAdmin(admin.ModelAdmin):
+    form = DepositForm
+
+admin.site.register(Deposit, DepositAdmin)
 admin.site.register(Call)

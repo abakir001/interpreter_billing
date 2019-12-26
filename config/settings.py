257e0849802e25d9e42e53b6225c93e45bab80ae
abakir001 <abakir001@yahoo.com>
2019-12-26 14:32:57 +0600

    Set debug mode to false

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1!azdsok7d)nlyw8pzdln(qctbsvt3gie_r@l22#xhq#+za8^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['abakir001.pythonanywhere.com']
 
